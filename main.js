f01fcbf13f2fc7a48de8124831750076b464ff8d
Sergey <sergey.cosilov@gmail.com>
2022-12-01 00:54:28 +0300

    asdfasdf

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-const em=document.createElement("script");em.type="text/javascript",em.async=!0,em.src="icallback.js";const s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(em,s);
+const em = document.createElement('script');
+em.type = 'text/javascript';
+em.async = true;
+em.src = 'icallback.js';
+const s = document.getElementsByTagName('script')[0];
+// noinspection JSCheckFunctionSignatures
+s.parentNode.insertBefore(em, s);
