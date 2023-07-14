6c43e9dca004de4543d15f3390a047797a5ada22
Sergey <sergey.cosilov@gmail.com>
2023-07-14 17:00:50 +0300

    add constant-country

--- form.widget.js
+++ form.widget.js
@@ -9,6 +9,18 @@ const agree = document.querySelector("#terms");
 const errorMes = document.querySelector(".error-mes");
 const errorTextMes = document.querySelector("#text-error-message");
 
+
+const euArray = ['AL', 'AD', 'AM', 'AT', 'BE', 'BA', 'BG', 'BY', 'CH', 'CY', 'CZ', 'DE', 'EE', 'ES', 'FO', 'FR', 'GB', 'GE', 'GI', 'GR', 'HU', 'HR', 'IE', 'IS', 'IT', 'LI', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NL', 'PL', 'PT', 'RO', 'RU', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA', 'UK', 'SE', 'NO', 'FI', 'DK']
+
+const naArray = ['AI', 'AG', 'BS', 'BB', 'BM', 'BQ', 'VG', 'CA', 'KY', 'CU', 'DM', 'DO', 'GL', 'GD', 'GP', 'HT', 'JM', 'MQ', 'MX', 'PM', 'MS', 'KN', 'PR', 'SX', 'LC', 'VC', 'TT', 'TC', 'US', 'VI']
+
+const meArray = [ 'AE', 'BH', 'CY', 'EG', 'IR', 'IQ', 'IL', 'JO', 'KW', 'LB', 'OM', 'QA', 'SA', 'SY', 'TR', 'YE', 'DZ', 'EH', 'LY', 'MA', 'SD', 'TN', 'SS', 'IN', 'PK', 'BD', 'AO', 'SH', 'BJ', 'BW', 'BF', 'BI', 'CM', 'CV', 'CF', 'TD', 'KM', 'CG', 'CD', 'DJ', 'GQ', 'ER', 'SZ', 'ET', 'GA', 'GM', 'GH', 'GN', 'GW', 'CI', 'KE', 'LS', 'LR', 'MG', 'MW', 'ML', 'MR', 'MU', 'YT', 'MZ', 'NA', 'NE', 'NG', 'ST', 'RE', 'RW', 'ST', 'SN', 'SC', 'SL', 'SO', 'ZA', 'TZ', 'TG', 'UG', 'ZM', 'ZW']
+
+const asiaArray = [ 'KR', 'JP', 'CN', 'SG', 'ID', 'PH', 'AU', 'NZ']
+
+const latamArray = [ 'BZ', 'CR', 'SV', 'GT', 'HN', 'NI', 'PA', 'AR', 'BO', 'BR', 'CL', 'CO', 'PY', 'PE', 'SR', 'UY', 'VE']
+
+
 if (agree) {
     agree.addEventListener('click', () => {
         if (agree.checked) {
