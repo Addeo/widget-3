8f5bd7daee413d57a2be74485fd1fbad615ebea1
Sergey <sergey.cosilov@gmail.com>
2023-07-21 16:36:51 +0300

    add script param-partner

---
+++ script-param-partner.js
@@ -0,0 +1,26 @@
+const paramPartner = localStorage.getItem('paramPartner');
+if (paramPartner) {
+    let affiliateffiliateField = document.getElementById("Affiliate")
+    if (affiliateffiliateField) {
+        affiliateffiliateField.value = paramPartner;
+    } else {
+        console.log('no Affiliate')
+    }
+} else {
+    console.log('no paramPartner')
+}
+
+var currentUrl = window.location.href;
+let fieldUrl = document.getElementById("url")
+if (fieldUrl) {
+    fieldUrl.value = currentUrl;
+} else {
+    console.log('no fieldUrl')
+}
+let titles = document.getElementsByTagName('title');
+let fieldPageTitle = document.getElementById("page_title")
+if (fieldPageTitle) {
+    fieldPageTitle.value = titles[0].innerHTML;
+} else {
+    console.log('no fieldPageTitle')
+}
