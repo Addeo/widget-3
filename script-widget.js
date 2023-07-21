d21254d1b1969894e2528fcdaa52fec955f6ad8f
Sergey <sergey.cosilov@gmail.com>
2023-07-21 15:47:37 +0300

    fix geo country with window.

--- script-widget.js
+++ script-widget.js
@@ -14,8 +14,8 @@ fetch('https://api.leads.convolo.ai/api/v1/support/check-ip/my')
     .then(function(response) {
     response.json().then(responseParse => {
         let countryCode = responseParse.ip.country
-        if (geo_forse_country) {
-            countryCode = geo_forse_country
+        if (window.geo_forse_country) {
+            countryCode = window.geo_forse_country
         }
 
         // WIDGET OPEN LOGIC
