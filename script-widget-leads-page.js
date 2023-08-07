d9976aa8b963baf291341d6114900601282791dc
Sergey <sergey.cosilov@gmail.com>
2023-08-07 17:13:34 +0300

    try fix

--- script-widget-leads-page.js
+++ script-widget-leads-page.js
@@ -28,8 +28,8 @@ fetch('https://api.leads.convolo.ai/api/v1/support/check-ip/my')
         window.countryCodeGlobal = countryCode.toLowerCase();
 
         // FORCE GEO PARAM
-        if (window.geo_forse_country) {
-            countryCode = window.geo_forse_country
+        if (geo_forse_country) {
+            countryCode = geo_forse_country
             window.countryCodeGlobal = countryCode;
         }
 
