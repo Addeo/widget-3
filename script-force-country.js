e305b99e1aff497f749cf4d515cdca00ef48ae3c
Sergey <sergey.cosilov@gmail.com>
2023-08-16 14:55:09 +0300

    delete logs

--- script-force-country.js
+++ script-force-country.js
@@ -1,6 +1,6 @@
 let geo_forse_country
 let params = (new URL(document.location)).searchParams;
-console.log('params', params)
+// console.log('params', params)
 geo_forse_country = params.get("geo_forse_country");
 
 // console.log('geo_forse_country', geo_forse_country)
