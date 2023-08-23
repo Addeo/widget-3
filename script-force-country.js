ec1681de293dd169d87bb6f0c97d594f4f479df5
Sergey <sergey.cosilov@gmail.com>
2023-08-23 12:51:37 +0300

    add isMobile

--- script-force-country.js
+++ script-force-country.js
@@ -8,3 +8,17 @@ if (geo_forse_country) {
     console.log('geo_forse_country', geo_forse_country)
     // console.log('geo_forse_country',geo_forse_country)
 }
+
+let isMobileApp = params.get("mobile-app");
+
+if (!isMobileApp) {
+    (function f() {
+        var em = document.createElement('script');
+        em.type = 'text/javascript';
+        em.async = true;
+        em.defer = true;
+        em.src = '//js.hs-scripts.com/7840017.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(em, s);
+    })();
+}
