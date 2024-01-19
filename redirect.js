b95c1d3db0f307ca54b5967eac785758aee47177
Sergey <sergey.cosilov@gmail.com>
2024-01-19 13:52:23 +0300

    test 2

--- redirect.js
+++ redirect.js
@@ -1,11 +1,9 @@
-console.log('redirect')
-console.log('redirect2')
 fetch('https://api.leads.convolo.ai/api/v1/support/check-ip/my')
     .then(function(response) {
         response.json().then(responseParse => {
             let countryCode = responseParse.ip.country
             console.log('countryCode', countryCode.toUpperCase())
-            if (['EG', 'PK', 'BD', 'IN', 'KW', 'BH', 'AE', 'SA', 'QA', 'OM', 'YE', 'RU'].includes(countryCode.toUpperCase())) {
+            if (['EG', 'PK', 'BD', 'IN', 'KW', 'BH', 'AE', 'SA', 'QA', 'OM', 'YE'].includes(countryCode.toUpperCase())) {
                 // window.location.href = "https://convolo.ai/";
                 window.location.replace("https://convolo.ai/");
             }
