6c41154679c36582a5559f6894978054ff3d2552
Sergey <sergey.cosilov@gmail.com>
2024-01-19 13:32:29 +0300

    fix some error

--- redirect.js
+++ redirect.js
@@ -5,17 +5,6 @@ fetch('https://api.leads.convolo.ai/api/v1/support/check-ip/my')
         response.json().then(responseParse => {
             let countryCode = responseParse.ip.country
 
-            // SET GLOBAL COUNTRY CODE
-            window.countryCodeGlobal = countryCode.toLowerCase();
-
-            // FORCE GEO PARAM
-            // console.log('geo_forse_country', geo_forse_country)
-            if (geo_forse_country) {
-                countryCode = geo_forse_country
-                window.countryCodeGlobal = countryCode;
-            }
-
-
             if (['EG', 'PK', 'BD', 'IN', 'KW', 'BH', 'AE', 'SA', 'QA', 'OM', 'YE', 'RU'].includes(countryCode.toUpperCase())) {
                 window.location.href = "https://convolo.ai/";
             }
