18cbc6c015599e43842c7093200583a72ef8c74f
Sergey <sergey.cosilov@gmail.com>
2024-03-29 13:40:59 +0300

    add nice select

--- script-replace-url.js
+++ script-replace-url.js
@@ -42,4 +42,28 @@ if (newUrl !== window.location.href ) {
     window.history.replaceState('', '', newUrl)
 }
 
-
+// <script>
+//     $(document).ready(function() {
+//     $('select').wrap('<div class="select_wrapper w-input"></div>')
+//     $('select').parent().prepend('<span>'+ $(this).find(':selected').text() +'</span>');
+//     $('select').parent().children('span').width($('select').width());
+//     $('select').css('display','none');
+//     $('select').parent().append('<ul class="select_inner"></ul>');
+//     $('select').children().each(function(){
+//     var opttext = $(this).text();
+//     var optval = $(this).val();
+//     $('select').parent().children('.select_inner').append('<li id="' + optval +'">' + opttext +'</li>');
+// });
+//
+//     $('select').parent().find('li').on('click',function (){
+//     var cur = $(this).attr('id');
+//     $('select').parent().children('span').text($(this).text());
+//     $('select').children().removeAttr('selected');
+//     $('select').children('[value="'+cur+'"]').attr('selected','selected');
+//     console.log($('select').children('[value="'+cur+'"]').text());
+// });
+//     $('select').parent().on('click',function (){
+//     $(this).find('ul').slideToggle('fast');
+// });
+// });
+// </script>
